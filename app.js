92c68654f94456820378852106a3289dfc978509
maheshchoudhary73 <maheshchoudhary7340@gmail.com>
2026-08-03 14:55:56 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -350,12 +350,6 @@ function initUserSocket(user) {
         }
     });
 
-                    sidebarPlanBadge.style.color = 'var(--accent)';
-                }
-            }
-        }
-    });
-
 
     socket.on('campaign_progress', (data) => {
         const { sent, pending, failed } = data;
