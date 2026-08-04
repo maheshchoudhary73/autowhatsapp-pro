a787a8121e3d2fb9c350847434e7e7431e315c92
maheshchoudhary73 <maheshchoudhary7340@gmail.com>
2026-08-04 08:15:16 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -370,9 +370,18 @@ function getUserQueueManager(uid) {
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
+// Force No Cache Header so Mobile Browsers (Chrome/Safari) Never Store Old HTML/JS
+app.use((req, res, next) => {
+    res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private');
+    res.set('Pragma', 'no-cache');
+    res.set('Expires', '0');
+    next();
+});
+
 // Serve static web app from root directory & public directory
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 
 // Socket.io Multi-Tenant Authenticated Connection Logic
